b9a59b4b3d3f1cfe502c45263a4160fb308138db
Akinola Abiodun Emmanuel <akinolaemmanuel49@gmail.com>
2024-08-04 06:18:31 +0100

    Add main dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <div className="flex min-h-full">
+      <div className="font-mono font-extrabold">Dashboard</div>
+      <div className="">
+        {/* Readonly list showing latest 10 links added by user */}
+      </div>
+    </div>
+  );
+}
