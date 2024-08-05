1e19807aa4717e1d2f4eaa9b6a39a755ae50b586
Akinola Abiodun Emmanuel <akinolaemmanuel49@gmail.com>
2024-08-05 06:56:59 +0100

    Conditional rendering

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,31 @@
-import Link from "next/link";
+"use client";
+
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const { user, error, isLoading } = useUser();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [user]);
+
   return (
     <header className="flex flex-col md:flex-row bg-white dark:bg-gray-800 justify-between p-4">
       <div className="flex h-8 font-bold text-left py-4 md:py-0 text-black dark:text-white hover:text-cyan-300">
         URL Shortener
       </div>
       <div className="flex md:flex-row flex-col gap-4">
-        <div className="text-gray-800 dark:text-white hover:text-cyan-300">
-          <a href="/api/auth/logout">Logout</a>
-        </div>
+        {isAuthenticated && (
+          <div className="text-gray-800 dark:text-white hover:text-cyan-300">
+            <a href="/api/auth/logout">Logout</a>
+          </div>
+        )}
       </div>
     </header>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
-import Image from "next/image";
+"use client";
 
-import PlaceHolderImage from "../public/media/images/PlaceHolder.jpeg";
+import Image from "next/image";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useEffect, useState } from "react";
+
+import PlaceHolderImage from "@/public/media/images/PlaceHolder.jpeg";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [user]);
+
   return (
     <div>
       <main className="flex flex-col">
-        {/* Hero Image should be here */}
+        {/* Hero Image */}
         <div style={{ width: "100%", height: "208px", position: "relative" }}>
           <Image
             src={PlaceHolderImage}
@@ -24,14 +39,18 @@ export default function Home() {
             Project Landing page
           </p>
           <p>Begin shortening your URLs.</p>
-          {/* This should be conditional, should show Go to Dashboard if user is authenticated. */}
-          {/* Conditonal Starts Here */}
+          {/* Conditional rendering based on authentication */}
           <div className="flex flex-col gap-4 mt-4">
-            <div className="border-b-2 rounded-lg bg-gray-800 text-white hover:text-cyan-600 px-12 py-4 font-mono font-bold text-lg">
-              <a href="/api/auth/login">Login</a>
-            </div>
+            {!isAuthenticated ? (
+              <div className="border-b-2 rounded-lg bg-gray-800 text-white hover:text-cyan-600 px-12 py-4 font-mono font-bold text-lg">
+                <a href="/api/auth/login">Login</a>
+              </div>
+            ) : (
+              <div className="border-b-2 rounded-lg bg-gray-800 text-white hover:text-cyan-600 px-12 py-4 font-mono font-bold text-lg">
+                <a href="/dashboard">Go to Dashboard</a>
+              </div>
+            )}
           </div>
-          {/* Conditonal Ends Here */}
         </div>
       </main>
     </div>
